966f6c63e1006c240b7aed08b42202abea7cf7e0
Yashaswini <yashaswini.s@capgemini.com>
2025-06-02 23:50:34 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 
+// import './App.css';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./components/home/Home"
+// import Favourites from './components/Favourites';
+// import Recipe from './components/recipe/Recipes';
+// import Login from './components/login/Login';
+// import Signup from './components/login/SignUp';
+// import Logout from './components/login/Logout';
+// import Header from "./components/header/Header";
+// import CuisineRecipes from "./components/recipe/cuisineRecipes";
+// import { AuthProvider } from './context/authContext';
+
+// function App() {
+//   return (
+//    <>
+// <AuthProvider>
+
+
+//  <Router>
+//    <Header/>
+//     <Routes>
+       
+//       <Route path='*' element={<Home/>}/>
+//       <Route path='/favourites' element={<Favourites/>}/>
+//       <Route path='/recipe/:id' element={<Recipe/>}/>
+//       <Route path="/cuisine/:cuisineType" element={<CuisineRecipes />} />
+//       <Route path='/login' element={<Login/>}/>
+//       <Route path='/signup' element={<Signup/>}/>
+//       <Route path='/logout' element={<Logout/>}/>
+//       </Routes>
+//   </Router>
+//    </AuthProvider>
+//    </>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/home/Home"
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./components/home/Home";
 import Favourites from './components/Favourites';
 import Recipe from './components/recipe/Recipes';
 import Login from './components/login/Login';
@@ -11,27 +50,34 @@ import Header from "./components/header/Header";
 import CuisineRecipes from "./components/recipe/cuisineRecipes";
 import { AuthProvider } from './context/authContext';
 
+function AppContent() {
+  const location = useLocation();
+  const hideHeaderRoutes = ["/login", "/signup"];
+
+  return (
+    <>
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/favourites' element={<Favourites />} />
+        <Route path='/recipe/:id' element={<Recipe />} />
+        <Route path="/cuisine/:cuisineType" element={<CuisineRecipes />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/logout' element={<Logout />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
 function App() {
   return (
-   <>
-<AuthProvider>
-
-
- <Router>
-   <Header/>
-    <Routes>
-       
-      <Route path='*' element={<Home/>}/>
-      <Route path='/favourites' element={<Favourites/>}/>
-      <Route path='/recipe/:id' element={<Recipe/>}/>
-      <Route path="/cuisine/:cuisineType" element={<CuisineRecipes />} />
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/logout' element={<Logout/>}/>
-      </Routes>
-  </Router>
-   </AuthProvider>
-   </>
+    <AuthProvider>
+      <Router>
+        <AppContent />
+      </Router>
+    </AuthProvider>
   );
 }
 
